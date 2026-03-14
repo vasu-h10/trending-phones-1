fb4ffcdf36f0bcb70779a9714ad198d71c8a1a0e
vasu-h10 <vasuh10@gmail.com>
2026-03-14 21:07:02 +0530

    Added offline caching for PWA

--- service-worker.js
+++ service-worker.js
@@ -1,5 +1,22 @@
+const CACHE = "trending-phones-v1";
+
+const FILES = [
+"/",
+"/index.html",
+"/style.css",
+"/app.js",
+"/phones.json",
+"/banner.jpg"
+];
+
 self.addEventListener("install", e => {
-self.skipWaiting();
+e.waitUntil(
+caches.open(CACHE).then(cache => cache.addAll(FILES))
+);
 });
 
-self.addEventListener("fetch", e => {});
+self.addEventListener("fetch", e => {
+e.respondWith(
+caches.match(e.request).then(r => r || fetch(e.request))
+);
+});
