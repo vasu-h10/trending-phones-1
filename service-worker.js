1e3d591f5c5cf1ef3d878d56c8e058cec685d0b4
Vasu-H <195313818+vasu-h10@users.noreply.github.com>
2026-03-19 08:11:26 +0530

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,22 +1,23 @@
-const CACHE = "trending-phones-v1";
+const CACHE = "trending-deals-v1";
 
 const FILES = [
-"/",
-"/index.html",
-"/style.css",
-"/app.js",
-"/phones.json",
-"/banner.jpg"
+  "/",
+  "/index.html",
+  "/style.css",
+  "/app.js",
+  "/categories.json"
 ];
 
+/* INSTALL */
 self.addEventListener("install", e => {
-e.waitUntil(
-caches.open(CACHE).then(cache => cache.addAll(FILES))
-);
+  e.waitUntil(
+    caches.open(CACHE).then(cache => cache.addAll(FILES))
+  );
 });
 
+/* FETCH */
 self.addEventListener("fetch", e => {
-e.respondWith(
-caches.match(e.request).then(r => r || fetch(e.request))
-);
+  e.respondWith(
+    fetch(e.request).catch(() => caches.match(e.request))
+  );
 });
