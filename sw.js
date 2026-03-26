e900595b8a823462d035cbce047ae65768fffbaf
Vasu-H <195313818+vasu-h10@users.noreply.github.com>
2026-03-26 10:05:27 +0530

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = "top10-phones-v4";
+const CACHE_NAME = "top10-phones-v6";
 
 const FILES_TO_CACHE = [
   "/",
@@ -6,7 +6,6 @@ const FILES_TO_CACHE = [
   "/style.css",
   "/app.js",
   "/mobiles.json",
-  "/banner.jpg",
   "/icon-192.png",
   "/icon-512.png"
 ];
@@ -14,9 +13,8 @@ const FILES_TO_CACHE = [
 /* INSTALL */
 self.addEventListener("install", event => {
   event.waitUntil(
-    caches.open(CACHE_NAME).then(cache => {
-      return cache.addAll(FILES_TO_CACHE);
-    })
+    caches.open(CACHE_NAME)
+      .then(cache => cache.addAll(FILES_TO_CACHE))
   );
   self.skipWaiting();
 });
@@ -40,9 +38,13 @@ self.addEventListener("activate", event => {
 /* FETCH */
 self.addEventListener("fetch", event => {
   event.respondWith(
-    caches.match(event.request).then(response => {
-      return response || fetch(event.request)
-        .catch(() => caches.match("/index.html"));
-    })
+    fetch(event.request)
+      .then(response => {
+        return caches.open(CACHE_NAME).then(cache => {
+          cache.put(event.request, response.clone());
+          return response;
+        });
+      })
+      .catch(() => caches.match(event.request))
   );
 });
